207b37eca90e8f5eaf5fdd84c9bab22bd97ab48e
晓月清岚 <1518014096@qq.com>
2024-08-17 16:44:16 +0800

    项目初始化，配置eslint，prettier

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  printWidth: 90,
+  endOfLine: 'lf',
+  proseWrap: 'preserve',
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  useTabs: false,
+  trailingComma: 'es5',
+  parser: 'typescript',
+}
